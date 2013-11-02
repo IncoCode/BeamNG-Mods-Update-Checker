c4882d265796d642b1d7a5b36fbf910724ecf479
IncoCode <akulich.dimon@gmail.com>
2013-11-02 21:44:40 +0200

    fixed #18

--- C#/BeamNGModsUpdateChecker/Program.cs
+++ C#/BeamNGModsUpdateChecker/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace BeamNGModsUpdateChecker
@@ -13,9 +16,26 @@ namespace BeamNGModsUpdateChecker
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault( false );
-            Application.Run( new frmMain() );
+            //Mutex mutex = new Mutex( false, appGuid );
+            //if ( !mutex.WaitOne( 0, false ) )
+            //{
+            //    MessageBox.Show( "Program already running!" );
+            //    return;
+            //}
+            using ( Mutex mutex = new Mutex( false, "Global\\" + appGuid ) )
+            {
+                if ( !mutex.WaitOne( 0, false ) )
+                {
+                    MessageBox.Show( "Program already running!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                    return;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault( false );
+                Application.Run( new frmMain() );
+            }
         }
+
+        private static string appGuid = "6759aa94-1382-4375-a900-b19e146a6010";
     }
 }
